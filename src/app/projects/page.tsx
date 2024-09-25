b762780b72be36673f7bb182a9be25b7ee514d37
derekcantillo <cantilloderek@gmail.com>
2024-09-25 00:23:51 -0500

    feat: create pages and components for example

---
+++ src/app/projects/page.tsx
@@ -0,0 +1,14 @@
+import { Button } from "components";
+import { NextPage } from "next";
+import React from "react";
+
+const Projects: NextPage = () => {
+  return (
+    <div>
+      Projects
+      <Button />
+    </div>
+  );
+};
+
+export default Projects;
