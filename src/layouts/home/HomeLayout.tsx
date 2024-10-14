2c0f2cb64fcfc8eb256fa72d9c53f1063242eee4
derekcantillo <cantilloderek@gmail.com>
2024-10-14 12:53:52 -0500

    Refactor page component and add HomeLayout

---
+++ src/layouts/home/HomeLayout.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import { HomeHeroSection } from 'layouts'
+
+export const HomeLayout = () => {
+	return (
+		<main>
+			<HomeHeroSection />
+		</main>
+	)
+}
---
+++ src/layouts/home/hero-section/HomeHeroSection.tsx
@@ -0,0 +1,5 @@
+import React from 'react'
+
+export const HomeHeroSection = (): JSX.Element => {
+	return <div>HomeHeroSection</div>
+}
