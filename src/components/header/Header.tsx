22aae3875eaae0f56fb58a1984e142253e8e2402
derekcantillo <cantilloderek@gmail.com>
2024-10-14 23:18:40 -0500

    Refactor header component layout

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -1,15 +1,21 @@
-import { routes } from 'lib'
+import { cn, routes } from 'lib'
 import Link from 'next/link'
 import React from 'react'
 
 export const Header = (): JSX.Element => {
 	return (
-		<div>
-			{routes.map(route => (
-				<Link key={route.name} href={route.path}>
-					<label>{route.name}</label>
-				</Link>
-			))}
+		<div className={cn('border flex justify-between', 'p-6 ')}>
+			<Link href="/">
+				<label>Derek</label>
+			</Link>
+			<div className={cn('flex gap-2')}>
+				{routes.map(route => (
+					<Link key={route.name} href={route.path}>
+						<label className={cn('text-lg')}>{route.name}</label>
+					</Link>
+				))}
+			</div>
+			<div className=""></div>
 		</div>
 	)
 }
